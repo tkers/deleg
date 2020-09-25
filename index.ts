5c83d38e789ccb2d60631e5ef312ba6699f67a5b
Tijn Kersjes <tijn@divbyzero.nl>
2020-09-25 18:57:35 +0200

    Add swap, drop, dup primitives

--- index.ts
+++ index.ts
@@ -8,6 +8,29 @@ type State = {
   stack: Stack;
 };
 
+// primitives
+const swap = (state: State): State => ({
+  ...state,
+  stack: [state.stack[1], state.stack[0], ...state.stack.slice(2)],
+});
+
+const dup = (state: State): State => ({
+  ...state,
+  stack: [state.stack[0], state.stack[0], ...state.stack.slice(1)],
+});
+
+const drop = (state: State): State => ({
+  ...state,
+  stack: state.stack.slice(1),
+});
+
+const primitives = {
+  swap,
+  dup,
+  drop,
+};
+
+// core
 const push = (value: Value, state: State): State => ({
   ...state,
   stack: [value, ...state.stack],
@@ -16,8 +39,11 @@ const push = (value: Value, state: State): State => ({
 export function executeWord(state: State, word: Word): State {
   if (typeof word === "number" || typeof word === "object") {
     return push(word, state);
+  } else if (word in primitives) {
+    return primitives[word](state);
+  } else {
+    throw new Error(`Unknown word: ${word}`);
   }
-  return state;
 }
 
 export function execute(state: State, words: Quotation): State {
@@ -41,5 +67,5 @@ const showStack = (state: State) =>
 
 const myState: State = { stack: [] };
 showStack(myState);
-const newState = execute(myState, [42, 5, sym("foo")]);
+const newState = execute(myState, [42, 5, sym("foo"), "swap", "drop", "dup"]);
 showStack(newState);
