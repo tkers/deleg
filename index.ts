b91f29b92e622ecd0facf6c65071ce55eabb12df
Tijn Kersjes <tijn@divbyzero.nl>
2020-09-25 18:50:26 +0200

    Add symbols

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-type Word = number | string;
+type Sym = { name: string };
+type Word = number | string | Sym;
 type Quotation = Array<Word>;
-type Value = number | string | Quotation;
-type Stack = Array<Value>;
 
+type Value = number | Sym;
+type Stack = Array<Value>;
 type State = {
   stack: Stack;
 };
@@ -13,7 +14,7 @@ const push = (value: Value, state: State): State => ({
 });
 
 export function executeWord(state: State, word: Word): State {
-  if (typeof word === "number") {
+  if (typeof word === "number" || typeof word === "object") {
     return push(word, state);
   }
   return state;
@@ -24,11 +25,21 @@ export function execute(state: State, words: Quotation): State {
 }
 
 /// test
+const sym = (name: string): Sym => ({ name });
+
+const showValue = (value: Value): string =>
+  typeof value === "number" ? `${value}` : `\\${value.name}`;
 
 const showStack = (state: State) =>
-  console.log(`<${state.stack.length}> ${state.stack.join(" ")}`);
+  console.log(
+    `<${state.stack.length}> ${state.stack
+      .slice()
+      .reverse()
+      .map(showValue)
+      .join(" ")}`
+  );
 
 const myState: State = { stack: [] };
 showStack(myState);
-const newState = execute(myState, [42, 5, 12]);
+const newState = execute(myState, [42, 5, sym("foo")]);
 showStack(newState);
